4b9cf136e27bb538d46ac6357dc4f3915194cf3d
smartinov <stefan.martinov@gmail.com>
2017-01-10 21:42:32 +0100

    Add basic routers for index and /books

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var express = require('express');
 var app = express();
 
 var port = 3000;
-app.listen(port, function (err) {
+app.get('/', function (req, res) {
+    res.send('Hello world');
+});
+
+app.get('/books', function (req, res) {
+    res.send('Hello Books');
+});
+
+app.listen(port, function () {
     console.log("running server on port " + port);
 });
