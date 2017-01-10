97fc96593014cb953c717b1ad89308df21ef6642
smartinov <stefan.martinov@gmail.com>
2017-01-10 22:01:28 +0100

    Add static resources to application

--- app.js
+++ app.js
@@ -3,9 +3,10 @@ var express = require('express');
 var app = express();
 
 var port = 3000;
-app.get('/', function (req, res) {
-    res.send('Hello world');
-});
+
+// Set public folder from root to be the static directory
+app.use(express.static('public'));
+app.use(express.static('src/views'));
 
 app.get('/books', function (req, res) {
     res.send('Hello Books');
