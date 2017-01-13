7df1e34077c7d4e99d97314989970efe976714f2
smartinov <stefan.martinov@gmail.com>
2017-01-13 13:25:22 +0100

    Add jshint and js source

--- app.js
+++ app.js
@@ -13,5 +13,5 @@ app.get('/books', function (req, res) {
 });
 
 app.listen(port, function () {
-    console.log("running server on port " + port);
+    console.log('running server on port ' + port);
 });
