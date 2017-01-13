9494cc10712e7c7a1649aaed14ae3c0d002b343c
smartinov <stefan.martinov@gmail.com>
2017-01-13 17:47:05 +0100

    Add injection of local dependencies from source files

--- gulpfile.js
+++ gulpfile.js
@@ -15,14 +15,23 @@ gulp.task('style', function () {
 
 gulp.task('inject', function () {
     var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+
+    var injectSrc = gulp.src(['./public/css/*.css', './public/js/*.js'], {read: false});
+
     var options = {
         bowerJson: require('./bower.json'),
         directory: './public/lib',
         ignorePath: '../../public/'
     };
 
+    var injectOptions = {
+        ignorePath: '../../public/'
+    };
+
     return gulp.src('./src/views/*.html')
         .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
 
 });
