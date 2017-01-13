ecd1f32e5677a27d569b9447e1118c1a9339c661
smartinov <stefan.martinov@gmail.com>
2017-01-13 13:58:34 +0100

    Add gulp 'style' task

---
+++ gulpfile.js
@@ -0,0 +1,11 @@
+var gulp = require('gulp');
+var jshint = require('gulp-jshint');
+var jsFiles = ['*.js', 'src/**/*.js'];
+
+gulp.task('style', function () {
+    gulp.src(jsFiles)
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish', {
+            verbose: true
+        }));
+});
